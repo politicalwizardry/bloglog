2bf80122a4ce252fc915dd0a68b553de82133453
politicalwizardry <wiley.snyder@gmail.com>
2019-11-09 23:20:35 -0500

    fc

--- server.py
+++ server.py
@@ -2,7 +2,7 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from os import curdir, sep
 
-PORT_NUMBER = 80
+PORT_NUMBER = 8000
 
 
 # This class will handles any incoming request from
